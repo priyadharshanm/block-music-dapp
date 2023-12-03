b287a5ea3c98a15495ec5032641145a9ee8020a0
Priyadharshan Manohar <priyadharshanmanohar@PriyadhshansMBP.lan>
2023-12-03 00:22:26 -0500

    sdsd

--- contracts/MusicPlatformInteractor.sol
+++ contracts/MusicPlatformInteractor.sol
@@ -35,6 +35,15 @@ contract MusicPlatformInteractor is ERC1155 {
         require(msg.sender == owner, "Caller is not the owner");
         _; 
     }
+    modifier validPrice(uint256 price) {
+    require(price > 0, "Price must be greater than zero");
+    _;
+    }
+    modifier royaltyRange(uint256 royaltyPercentage) {
+    require(royaltyPercentage >= 0 && royaltyPercentage <= 100, "Royalty percentage out of range");
+    _;
+}
+
 
     struct Album {
         uint256 id;
@@ -60,7 +69,7 @@ contract MusicPlatformInteractor is ERC1155 {
 
  
     // Artist adds a new album and sets its price
-    function addNewAlbum(string memory albumName, string memory artistName, uint256 price, string memory uri) external {
+    function addNewAlbum(string memory albumName, string memory artistName, uint256 price, string memory uri) validPrice(price){
         Album memory newAlbum = Album({
             id: nextAlbumId,
             artist: msg.sender,
@@ -86,7 +95,7 @@ contract MusicPlatformInteractor is ERC1155 {
     }
 
     // Artist adds an exclusive album represented by a MasterPieceToken
-    function addExclusiveAlbum(string memory albumName, string memory artistName, uint256 price, uint256 royaltyPercentage, string memory uri) external returns (uint256) {
+    function addExclusiveAlbum(string memory albumName, string memory artistName, uint256 price, uint256 royaltyPercentage, string memory uri) external returns (uint256) royaltyRange(royaltyPercentage){
         uint256 tokenId = masterpieceToken.mintMasterpieceToken(msg.sender, uri);
         exclusiveAlbumIds.push(tokenId);
         ExclusiveAlbum memory newAlbum = ExclusiveAlbum({
